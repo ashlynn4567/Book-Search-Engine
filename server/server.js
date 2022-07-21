0e0e4215bb4839b983cda4ea2993dbb578b2caff
Ashley's PC <ashlynn4567@gmail.com>
2022-07-21 00:21:01 -0500

    bug fixes

--- server/server.js
+++ server/server.js
@@ -2,17 +2,17 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 const db = require("./config/connection");
-
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers, 
   context: authMiddleware,
+  persistedQueries: false, 
 });
 
 app.use(express.urlencoded({ extended: true }));
